8d9547c89a4fe7104ad79c1e00bbea353e928a38
dgallego <dgallego@VGPDGALLEGO.kuka.es>
2019-04-25 11:59:22 +0200

    --- automatic commit on synchronization ---

---
+++ src/setUp/TransportPosition.java
@@ -0,0 +1,38 @@
+package setUp;
+
+import javax.inject.Inject;
+
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.motionModel.PTP;
+import com.kuka.roboticsAPI.uiModel.ApplicationDialogType;
+
+public class TransportPosition extends RoboticsAPIApplication {
+	@Inject
+	private LBR lbr;
+	
+	private final static String informationText=
+         "This application is intended for floor mounted robots!"+ "\n" +
+			"\n" +
+			"The robot moves to the transportation position.";
+
+	public void initialize() 
+	{
+	}
+
+	public void run() {
+		getLogger().info("Show modal dialog and wait for user to confirm");
+        int isCancel = getApplicationUI().displayModalDialog(ApplicationDialogType.QUESTION, informationText, "OK", "Cancel");
+        if (isCancel == 1)
+        {
+            return;
+        }
+
+		getLogger().info("Move to the transport position");
+		PTP ptpToTransportPosition = ptp(0, Math.toRadians(25), 0, Math.toRadians(90), 0, 0, 0);
+		ptpToTransportPosition.setJointVelocityRel(0.25);
+		lbr.move(ptpToTransportPosition);
+	}
+
+}
