beac5872588269eb2b56eba8f95bb5729709b689
base <base@XPS>
2019-05-26 15:13:12 +0200

    --- automatic rebase to remote; old local SHA: 67a0608bb3c6e78f4215f58ebec531f8e56453f3 ---

--- src/application/PRC_RunUDP.java
+++ src/application/PRC_RunUDP.java
@@ -74,7 +74,7 @@ public class PRC_RunUDP extends RoboticsAPIApplication {
 		//AbstractIOGroup iogrp = iogrp;
 		
 		try {
-			prc_udp.CORE_UDP(robot, kuka_Sunrise_Cabinet_1, getApplicationData().createFromTemplate(toolname), tcpname, baseFrame, enablellogging, getLogger(), getApplicationData(), null, "172.31.1.148", 49152);
+			prc_udp.CORE_UDP(robot, kuka_Sunrise_Cabinet_1, getApplicationData().createFromTemplate(toolname), tcpname, baseFrame, enablellogging, getLogger(), getApplicationData(), null, "172.31.1.149", 49152);
 		} catch (SocketException e) {
 			// TODO Automatisch generierter Erfassungsblock
 			e.printStackTrace();
