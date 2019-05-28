7934b4ff4429fa29fc881cc1d8edec0f1906289c
base <base@XPS>
2019-05-28 11:34:59 +0200

    --- automatic rebase to remote; old local SHA: 8c5ba42c2af01a428092c78e759cf1f3f5837910 ---

--- src/application/PRC_RunSmartServo.java
+++ src/application/PRC_RunSmartServo.java
@@ -67,7 +67,7 @@ public class PRC_RunSmartServo extends RoboticsAPIApplication {
 	@Override
 	public void run() {
 		LBR robot = lbr_iiwa; //SET ROBOT
-		String toolname = "Tool"; //SET TOOL NAME
+		String toolname = "IAACGripper"; //SET TOOL NAME
 		String tcpname = "TCP"; //SET TCP NAME
 		ObjectFrame baseFrame = getApplicationData().getFrame("/BASE1"); //OPTIONAL: SET BASE COORDINATE SYSTEM
 		boolean enablellogging = true; //OPTIONAL: ENABLE CONSOLE LOGGING
