dcc8d7cf9953e277a1ced79866027eba0b7aff39
Ardoo <Ardoo@DESKTOP-7F5M04H>
2019-06-20 16:31:30 +0200

    --- automatic rebase to remote; old local SHA: cb5ff4ca4f5dfae3641708e7197216fb28d0e9d1 ---

--- src/application/Scan_Pick.java
+++ src/application/Scan_Pick.java
@@ -316,7 +316,7 @@ public class Scan_Pick extends RoboticsAPIApplication {
 		JointPosition jointPosition = new JointPosition(-Math.PI/2,0,0,0,Math.PI/2,Math.PI/2,0);
 		actTCP.move(ptp(jointPosition).setJointVelocityRel(.5));
 		
-		jointPosition = new JointPosition(-2*Math.PI/3, 0.2, 0, -Math.PI/2+0.2, Math.PI/2, Math.PI/2, -Math.PI/2);
+		jointPosition = new JointPosition(-2*Math.PI/3, 0.2, 0, -Math.PI/2+0.2, 0, Math.PI/2, -Math.PI/2);
 		actTCP.move(ptp(jointPosition).setJointVelocityRel(.5));
 		
 		ThreadUtil.milliSleep(2000);
